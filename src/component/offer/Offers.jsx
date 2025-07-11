121ab0f0460de7927d2e3f5f4597a4a8c6ed8731
Salami toyosi <salamitoyosikabiru@gmail.com>
2025-07-11 23:22:11 +0100

    working on the banner

--- src/component/offer/Offers.jsx
+++ src/component/offer/Offers.jsx
@@ -2,66 +2,72 @@ import { Button, Typography, useMediaQuery, useTheme } from '@mui/material'
 import React from 'react'
 import exclusive_image from "../../Assets/exclusive_image.png"
 
+
 const Offers = () => {
-    const isMobile = useMediaQuery(useTheme().breakpoints.down("md"))
-    return (
-        <div className='offer' style={{
-            width: isMobile ? 20 : 65,
-            height: isMobile ? "30vh" : "60vh",
+    const isMobile= useMediaQuery(useTheme().breakpoints.down("sm"))
+  return (
+    <div className='offer' style={{
+        width:isMobile? "auto": "65%",
+        height:isMobile? "auto" :"60vh",
+        display: "flex",
+      
+        padding:isMobile? "5xp":"0px 140px",
+        margin: isMobile? "2px":"100px",
+        alignItems: "center",
+        justifyContent: "center"
+
+       
+    }}>
+        <div style={{
+            flex: 1,
             display: "flex",
-
-            padding: "0px 140px",
-            margin: "100px"
-
-
+            flexDirection: "column",
+            justifyItems: "center",
+            alignItems: "center"
         }}>
-            <div style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                justifyItems: "center"
-            }}>
-                <Typography sx={{
-                    fontSize:isMobile? 20: 80,
-                    fontWeight: 600,
-                    color: "#171717"
-                }}>Exclusive</Typography>
-                <Typography sx={{
-                    fontSize: "80px",
-                    fontWeight: 600,
-                    color: "#171717"
-                }}>Offers For You</Typography>
-                <Typography sx={{
-                    fontSize: "22px",
-                    fontWeight: 600,
-                    color: "#171717"
-                }}>ONLY ON BEST SELLERS PRODUCTS</Typography>
-                <Button variant='contained' sx={{
-                    width: "282px",
-                    height: "70px",
-                    borderRadius: "35px",
-                    background: "#ff4141",
-                    border: "none",
-                    color: "#fff",
-                    fontSize: "22px",
-                    fontWeight: 500,
-                    cursor: "pointer",
-                    marginTop: "30px"
-                }}>Check now</Button>
-            </div>
-            <div style={{
-                flex: 1,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "flex-end",
-                paddingTop: 50
-            }}>
-                <img src={exclusive_image} alt="" />
-
-            </div>
+            <Typography fontSize={{xs:30, md:60}} sx={{
+               
+                fontWeight: 600,
+                color: "#171717"
+            }}>Exclusive</Typography>
+            <Typography fontSize={{xs:30, md:50}} sx={{
+                
+                fontWeight: 600,
+                color: "#171717"
+            }}>Offers For You</Typography>
+            <Typography fontSize={{sx:15, md:20}} sx={{
+              
+                fontWeight: 600,
+                color: "#171717"
+            }}>ONLY ON BEST SELLERS PRODUCTS</Typography>
+            <Button variant='contained'sx={{
+                width: isMobile? "150px":"282px",
+                height: isMobile? "50px":"70px",
+                borderRadius: "35px",
+                background: "#ff4141",
+                border: "none",
+                color: "#fff",
+                fontSize:isMobile? "15px": "22px",
+                fontWeight: 500,
+                cursor: "pointer",
+                marginTop: "30px"
+            }}>Check now</Button>
+        </div>
+        <div style={{
+            flex: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            paddingTop: 50
+        }}>
+            <img style={{
+                width: isMobile? "150px":"400px"
+            }} src={exclusive_image} alt="" />
 
         </div>
-    )
+
+    </div>
+  )
 }
 
 export default Offers
